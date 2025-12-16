8575012fc61e90f51bedd32b3508dfac618154f2
aeonik <aeonik.chaos@gmail.com>
2025-12-16 14:45:58 -0500

    chore(dir-locals): add .dir-locals.el for CIDER

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil
+  . ((cider-preferred-build-tool . clojure-cli)
+     (cider-default-cljs-repl . shadow)
+     (cider-shadow-default-options . "app")
+     (cider-shadow-watched-builds . ("app"))
+     (cider-offer-to-open-cljs-app-in-browser . nil))))
